6e339bbe06e6b7f2e3c6ae63f69e9747ba675df1
Shahar saadon <shahars@moveo.co.il>
2023-07-23 14:29:39 +0300

    initial project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import '../src/Assets/Styles/global.scss'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,10 +8,11 @@ function App() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+<h2>
+  vite logo
+  </h2>        </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+    <h1>react logo</h1>
         </a>
       </div>
       <h1>Vite + React</h1>
