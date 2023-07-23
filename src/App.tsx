d8d25ce7ed0b41dbbf0a0fd57e7f087fb0190366
Shahar saadon <shahars@moveo.co.il>
2023-07-23 16:32:51 +0300

    /app-header

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,13 @@
 import { useState } from 'react'
 import '../src/Assets/Styles/global.scss'
-
+import {AppHeader} from './Components/AppHeader'
+import {AppFooter} from './Components/AppFooter'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-<h2>
-  vite logo
-  </h2>        </a>
-        <a href="https://react.dev" target="_blank">
-    <h1>react logo</h1>
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+  <AppHeader/>
     </>
   )
 }
