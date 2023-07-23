d205687f494aeb3e4862238f427717b740fd6bf3
Shahar saadon <shahars@moveo.co.il>
2023-07-23 17:11:58 +0300

    /AppFooter

---
+++ src/Components/AppFooter.tsx
@@ -0,0 +1,11 @@
+export const AppFooter = () => {
+  return (
+    <footer>
+        <ul className="clean-list flex flex-column">
+            <li>Contact Us</li>
+            <li>Term of Use</li>
+            <li>Privacy Policy</li>
+        </ul>
+    </footer>
+  )
+}
