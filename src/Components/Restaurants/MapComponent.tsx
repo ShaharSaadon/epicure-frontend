2486e42cbdbb57cc9eae7af2158617537dd5f698
Shahar Saadon <shahars@moveo.co.il>
2023-07-30 14:47:31 +0300

    /restaurantIndex - fixing liks

--- src/Components/Restaurants/MapComponent.tsx
+++ src/Components/Restaurants/MapComponent.tsx
@@ -1,18 +1,5 @@
 import React from "react";
-import { GoogleMap, Marker } from "react-google-maps";
-
-const MyMapComponent = (props) => (
-    <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
-        {props.isMarkerShown && (
-            <Marker position={{ lat: -34.397, lng: 150.644 }} />
-        )}
-    </GoogleMap>
-);
 
 export const MapComponent = () => {
-    return (
-        <div>
-            <MyMapComponent isMarkerShown={false} />
-        </div>
-    );
+    return <div>MapComponent </div>;
 };
